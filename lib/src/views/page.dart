e50bcf994a2418be92991c932dcf28ea07030306
Matt Smith <matt.smith@trof.me>
2021-04-05 15:06:22 +0100

    bug fix2

--- lib/src/views/page.dart
+++ lib/src/views/page.dart
@@ -15,24 +15,22 @@ class OnboardPage extends StatelessWidget {
   final TextStyle titleStyle;
   final TextStyle infoStyle;
   final EdgeInsets infoPadding;
-  final Widget imageWidget;
 
-  const OnboardPage(
-      {Key key,
-      this.pageModel,
-      this.background,
-      this.index,
-      this.pagesLength,
-      this.dragPercent,
-      this.pagesPadding,
-      this.pagesImageColor,
-      this.titleAndInfoPadding,
-      this.titleAndInfoHeight,
-      this.titleStyle,
-      this.infoStyle,
-      this.infoPadding,
-      this.imageWidget})
-      : super(key: key);
+  const OnboardPage({
+    Key key,
+    this.pageModel,
+    this.background,
+    this.index,
+    this.pagesLength,
+    this.dragPercent,
+    this.pagesPadding,
+    this.pagesImageColor,
+    this.titleAndInfoPadding,
+    this.titleAndInfoHeight,
+    this.titleStyle,
+    this.infoStyle,
+    this.infoPadding,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final singlePageScrollPercentage = dragPercent / (1 / pagesLength);
@@ -47,7 +45,7 @@ class OnboardPage extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: imageWidget,
+              child: pageModel.imageWidget,
             ),
             Container(
               padding: titleAndInfoPadding,
